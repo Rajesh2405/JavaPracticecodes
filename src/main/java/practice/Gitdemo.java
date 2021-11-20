f0675b815e1dd94108486827d5820abf264ea5ea
Rajesh <rajesm2407@gmail.com>
2021-11-20 18:54:27 +0530

    Adhoc changes from version 2

---
+++ src/main/java/practice/Gitdemo.java
@@ -0,0 +1,18 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class Gitdemo {
+
+@Test	
+public void phones() {
+	
+	System.out.println("Iphone");
+	System.out.println("IphoneMax pro");
+	System.out.println("One plus");
+	System.out.println("Samsung");
+}	
+
+
+	
+}
