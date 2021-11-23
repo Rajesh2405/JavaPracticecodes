a32095af84aaec7669b08b4a717f8eb3bbfd2948
Rajesh <rajesm2407@gmail.com>
2021-11-23 11:12:08 +0530

    New programe added

---
+++ src/main/java/coreJava/ArraytoString.java
@@ -0,0 +1,18 @@
+package coreJava;
+
+import java.util.Arrays;
+
+public class ArraytoString {
+
+	public static void main(String[] args) {
+		
+		char []arr	= {'R','a','j','e','s','h'};
+		
+		String arrtostring = Arrays.toString(arr);
+		
+		System.out.println(arrtostring);
+		
+		
+	}
+
+}
